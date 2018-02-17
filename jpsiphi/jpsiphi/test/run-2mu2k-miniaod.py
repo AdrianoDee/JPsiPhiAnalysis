7aac6d10c7f6e561e328409ad6b785b29d928718
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-02-17 21:54:02 +0100

    Fixing globaTags

--- jpsiphi/jpsiphi/test/run-2mu2k-miniaod.py
+++ jpsiphi/jpsiphi/test/run-2mu2k-miniaod.py
@@ -10,19 +10,20 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condD
 process.load("SimTracker.TrackerHitAssociation.tpClusterProducer_cfi")
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, '80X_dataRun2_2017LegacyRepro_v4')
-#process.GlobalTag = GlobalTag(process.GlobalTag, '80X_dataRun2_Prompt_v16')
 
-
-#Global Tags 2016 ReReco Nov17 CMSSW_9_4_0
-#/Charmonium/Run2017*-17Nov2017-*/MINIAOD -> 9_4_0
-# BCDE -> 94X_dataRun2_ReReco_EOY17_v1
-# F -> 94X_dataRun2_ReReco_EOY17_v2
+#Global Tags 2016 ReReco Aug17 CMSSW_8_0_29
+# /Charmonium/Run2016*-07Aug17*/MINIAOD
+# BCDEFGH -> 80X_dataRun2_2016LegacyRepro_v4
 # Dataset:
-#  /Charmonium/Run2017C-17Nov2017-v1/MINIAOD
-#  /Charmonium/Run2017D-17Nov2017-v1/MINIAOD
-#  /Charmonium/Run2017E-17Nov2017-v1/MINIAOD
-#  /Charmonium/Run2017F-17Nov2017-v1/MINIAOD
-#  /Charmonium/Run2017B-17Nov2017-v1/MINIAOD
+# /Charmonium/Run2016B-07Aug17_ver2-v1/MINIAOD
+# /Charmonium/Run2016C-07Aug17-v1/MINIAOD
+# /Charmonium/Run2016D-07Aug17-v1/MINIAOD
+# /Charmonium/Run2016E-07Aug17-v1/MINIAOD
+# /Charmonium/Run2016F-07Aug17-v1/MINIAOD
+# /Charmonium/Run2016G-07Aug17-v1/MINIAOD
+# /Charmonium/Run2016H-07Aug17-v1/MINIAOD
+# /Charmonium/Run2016B-07Aug17_ver1-v1/MINIAOD
+
 
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True))
 
