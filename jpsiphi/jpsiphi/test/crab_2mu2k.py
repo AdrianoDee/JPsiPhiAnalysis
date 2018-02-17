c34b649db16f2afd286b0d6fc82ccac413db7bb3
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-02-17 23:21:02 +0100

    Crab 2016 datasets

--- jpsiphi/jpsiphi/test/crab_2mu2k.py
+++ jpsiphi/jpsiphi/test/crab_2mu2k.py
@@ -33,12 +33,18 @@ sites = ['T2_AT_Vienna', 'T2_BE_IIHE', 'T2_BE_UCL', 'T2_BR_SPRACE', 'T2_BR_UERJ'
  'T3_US_R*', 'T3_US_S*', 'T3_US_T*', 'T3_US_UCD', 'T3_US_UCR', 'T3_US_UCSB',
  'T3_US_UMD']
 
+
 datasetnames = {
-"B" :  datasetbase + "/Run2017B-17Nov2017-v1/MINIAOD",
-"C" :  datasetbase + "/Run2017C-17Nov2017-v1/MINIAOD",
-"D" :  datasetbase + "/Run2017D-17Nov2017-v1/MINIAOD",
-"E" :  datasetbase + "/Run2017E-17Nov2017-v1/MINIAOD",
-"F" :  datasetbase + "/Run2017F-17Nov2017-v1/MINIAOD",
+
+    "B1": datasetbase + "/Run2016B-07Aug17_ver2-v1/MINIAOD"
+    "B2": datasetbase + "/Run2016B-07Aug17_ver1-v1/MINIAOD"
+    "C" :  datasetbase + "/Run2016C-07Aug17-v1/MINIAOD"
+    "D" :  datasetbase + "/Run2016D-07Aug17-v1/MINIAOD"
+    "E" :  datasetbase + "/Run2016E-07Aug17-v1/MINIAOD"
+    "F" :  datasetbase + "/Run2016F-07Aug17-v1/MINIAOD"
+    "G" :  datasetbase + "/Run2016G-07Aug17-v1/MINIAOD"
+    "H" :  datasetbase + "/Run2016H-07Aug17-v1/MINIAOD"
+
 }
 
 runNumber = [
