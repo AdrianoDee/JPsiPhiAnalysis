8f55fc0d08cb8cfd6f197142ee22a75221ee4566
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-02-19 01:25:35 +0100

    String cut objects

--- jpsiphi/jpsiphi/interface/DiMuonProducer.h
+++ jpsiphi/jpsiphi/interface/DiMuonProducer.h
@@ -51,17 +51,17 @@ class DiMuonProducerPAT : public edm::EDProducer {
   // ----------member data ---------------------------
  private:
 
-
-  // ;
-
   // edm::EDGetTokenT<reco::TrackCollection> revtxtrks_;
   // edm::EDGetTokenT<reco::BeamSpot> revtxbs_;
-  // StringCutObjectSelector<pat::Muon> higherPuritySelection_;
-  // StringCutObjectSelector<pat::Muon> lowerPuritySelection_;
-  // StringCutObjectSelector<reco::Candidate, true> dimuonSelection_;
+
   std::string muons_;
-  std::string thebeamspot_
+  std::string thebeamspot_;
   std::string thePVs_;
+
+  StringCutObjectSelector<pat::Muon> higherPuritySelection_;
+  StringCutObjectSelector<pat::Muon> lowerPuritySelection_;
+  StringCutObjectSelector<reco::Candidate, true> dimuonSelection_;
+
   bool addCommonVertex_, addMuonlessPrimaryVertex_;
   bool resolveAmbiguity_;
   bool addMCTruth_;
