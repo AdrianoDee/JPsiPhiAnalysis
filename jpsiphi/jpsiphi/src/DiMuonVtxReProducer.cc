fafc5b0d236e5936b1e02764d4173a5ade122ee2
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-02-19 15:22:34 +0100

    Fixing VertexReproducer

--- jpsiphi/jpsiphi/src/DiMuonVtxReProducer.cc
+++ jpsiphi/jpsiphi/src/DiMuonVtxReProducer.cc
@@ -10,50 +10,38 @@
 
 DiMuonVtxReProducer::DiMuonVtxReProducer(const edm::Handle<reco::VertexCollection> &handle, const edm::Event &iEvent)
 {
-    const edm::Provenance *prov = handle.provenance();
-    if (prov == nullptr) throw cms::Exception("CorruptData") << "Vertex handle doesn't have provenance.";
-    edm::ParameterSet psetFromProvenance = edm::parameterSet(*prov);
+  
+  const edm::Provenance *prov = handle.provenance();
+  if (prov == 0) throw cms::Exception("CorruptData") << "Vertex handle doesn't have provenance.";
+  edm::ParameterSetID psid = prov->psetID();
+  edm::pset::Registry *psregistry = edm::pset::Registry::instance();
+  edm::ParameterSet psetFromProvenance;
+  if ( !psregistry->getMapped(psid, psetFromProvenance) )
+      throw cms::Exception("CorruptData") << "Vertex handle parameter set ID id = " << psid;
+  if ( prov->moduleName() != "PrimaryVertexProducer" )
+      throw cms::Exception("Configuration") << "Vertices to re-produce don't come from a PrimaryVertexProducer, but from a " << prov->moduleName() <<".\n";
 
-    bool is_primary_available = false;
-    const edm::Provenance *parent_prov = prov;
-    if (edm::moduleName(*prov) != "PrimaryVertexProducer") {
-        std::vector<edm::BranchID> parents = prov->productProvenance()->parentage().parents();
-        for (std::vector<edm::BranchID>::const_iterator it = parents.begin(), ed = parents.end(); it != ed; ++it) {
-           edm::Provenance parprov = iEvent.getProvenance(*it);
-           if (parprov.friendlyClassName() == "recoVertexs") {  // for AOD actually this the parent we should look for
-              parent_prov = &parprov;
-              psetFromProvenance = edm::parameterSet(parprov);
-              is_primary_available = true;
-              break;
-           }
-        }
-    } else is_primary_available = true;
-    if (is_primary_available) prov = parent_prov;
-    else throw cms::Exception("Configuration") << "Vertices to re-produce don't come from a PrimaryVertexProducer \n";
+  configure(psetFromProvenance);
 
-    configure(psetFromProvenance);
-
-    // Now we also dig out the ProcessName used for the reco::Tracks and reco::Vertices
-    std::vector<edm::BranchID> parents = prov->productProvenance()->parentage().parents();
-    bool foundTracks = false;
-    bool foundBeamSpot = false;
-    for (std::vector<edm::BranchID>::const_iterator it = parents.begin(), ed = parents.end(); it != ed; ++it) {
-        edm::Provenance parprov = iEvent.getProvenance(*it);
-        if (parprov.friendlyClassName() == "recoTracks") {
-            tracksTag_ = edm::InputTag(parprov.moduleLabel(), parprov.productInstanceName(), parprov.processName());
-            foundTracks = true;
-            if (parprov.moduleLabel() != "generalTracks") foundTracks = false;  // this is necessary since we are asking for that in onia2mumu
-        } else if (parprov.friendlyClassName() == "recoBeamSpot") {
-            beamSpotTag_ = edm::InputTag(parprov.moduleLabel(), parprov.productInstanceName(), parprov.processName());
-            foundBeamSpot = true;
-            if (parprov.moduleLabel() != "offlineBeamSpot") foundBeamSpot = false;   // this is necessary since we are asking for that in onia2mumu
-        }
-    }
-    if (!foundTracks || !foundBeamSpot) {
-       //edm::LogWarning("OniaVtxReProducer_MissingParentage") <<
-       throw cms::Exception("Configuration") <<
-       "Can't find correct parentage info for vertex collection inputs: " <<  (foundTracks ? "" : "generalTracks ") << (foundBeamSpot ? "" : "offlineBeamSpot") << "\n";
-    }
+  // Now we also dig out the ProcessName used for the reco::Tracks and reco::Vertices
+  std::vector<edm::BranchID> parents = prov->parents();
+  bool foundTracks = false;
+  bool foundBeamSpot = false;
+  for (std::vector<edm::BranchID>::const_iterator it = parents.begin(), ed = parents.end(); it != ed; ++it) {
+      edm::Provenance parprov = iEvent.getProvenance(*it);
+      if (parprov.friendlyClassName() == "recoTracks") {
+          tracksTag_ = edm::InputTag(parprov.moduleLabel(), parprov.productInstanceName(), parprov.processName());
+          foundTracks = true;
+      } else if (parprov.friendlyClassName() == "recoBeamSpot") {
+          beamSpotTag_ = edm::InputTag(parprov.moduleLabel(), parprov.productInstanceName(), parprov.processName());
+          foundBeamSpot = true;
+      }
+  }
+  if (!foundTracks || !foundBeamSpot) {
+      edm::LogWarning("VertexReProducer_MissingParentage") <<
+          "Can't find parentage info for vertex collection inputs: " <<
+          (foundTracks ? "" : "tracks ") << (foundBeamSpot ? "" : "beamSpot") << "\n";
+  }
 
 }
 
