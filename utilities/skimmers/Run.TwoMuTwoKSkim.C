a144f44a225749c51dd781768f217a199a17e15a
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-02-27 16:43:34 +0100

    Small fix

--- utilities/skimmers/Run.TwoMuTwoKSkim.C
+++ utilities/skimmers/Run.TwoMuTwoKSkim.C
@@ -8,7 +8,7 @@
 
   // INPUT DATA SAMPLE ON LOCAL DISK
 
-  TDSet* dataset = new TDSet("TTree", "DiMuon PiK Tree", "rootuplePiK");
+  TDSet* dataset = new TDSet("TTree", "JPsi Phi Tree", "rootuple");
   //
   dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/2mu2k_BCDEF_2017.root");
 
