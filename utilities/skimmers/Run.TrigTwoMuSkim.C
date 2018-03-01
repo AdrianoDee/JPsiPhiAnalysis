6a7bbb1bfaf687a80c523e90b3133cde0e310fcb
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-03-01 11:20:13 +0100

    Directories and Run files

---
+++ utilities/skimmers/Run.TrigTwoMuSkim.C
@@ -0,0 +1,26 @@
+{
+
+#include "TTree.h"
+#include "TDSet.h"
+#include "TProof.h"
+#include "TString.h"
+
+
+  // INPUT DATA SAMPLE ON LOCAL DISK
+
+  TDSet* dataset = new TDSet("TTree", "dimuonTree", "DiMuonRootuplerHLT");
+  //
+  dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/crab_miniaod_2k2Trig_Charmonium_Run2017F-17Nov2017-v1_MINIAOD_305388-309000__20180301_001621/merge.root");
+
+  // dataset->Add("/Users/adrianodiflorio/Documents/Git/X4140/ProofLite/Y4140_testrootuple.root");
+  TString selector = "TrigTwoMuSkim";
+  TProof *p = TProof::Open("workers=40"); // 12 workers for qsub
+
+  // Processing
+  cout << ">> Processing " << selector << " ... " << endl;
+
+  TString selectorplus = selector;
+  selectorplus += ".C+";
+  p->Process(dataset, selectorplus);
+
+}
