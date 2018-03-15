310c91f72e153e6148781a7e8d6f8e78623c7d77
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-03-15 16:00:58 +0100

    PrintLevel

--- utilities/skimmers/2mu2k/Run.TwoMuTwoKSkim.C
+++ utilities/skimmers/2mu2k/Run.TwoMuTwoKSkim.C
@@ -11,10 +11,10 @@
   TDSet* dataset = new TDSet("TTree", "JPsi Phi Tree", "rootuple");
   //
   //dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/2mu2k_miniaod_17Nov2017_BCDEF_2017.root");
-  dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/2mu2k_miniaod_07Aug17_BCDEFGH_2016.root");
+  //dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/2mu2k_miniaod_07Aug17_BCDEFGH_2016.root");
   //phi mass window [0.97-1.06]
   dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/2mu2k_miniaod_07Aug17_BCDEFGH_2016_phi_097_106.root");
-  dataset->Add("/lustre/cms/store/user/adiflori/Charmonium//2mu2k_miniaod_17Nov2017_BCDEF_2017_phi_097_106.root");
+  //dataset->Add("/lustre/cms/store/user/adiflori/Charmonium//2mu2k_miniaod_17Nov2017_BCDEF_2017_phi_097_106.root");
 
   TString selector = "/lustre/home/adrianodif/jpsiphi/analysis/utilities/skimmers/2mu2k/TwoMuTwoKSkim";
   TProof *p = TProof::Open("workers=40"); // 12 workers for qsub
