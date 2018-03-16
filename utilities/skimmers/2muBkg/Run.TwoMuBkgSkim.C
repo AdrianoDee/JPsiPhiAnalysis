ff0bb24efc85d152a8e1ab6c124882ac9e29ae06
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-03-16 04:21:58 +0100

    Up 2muBkg

--- utilities/skimmers/2muBkg/Run.TwoMuBkgSkim.C
+++ utilities/skimmers/2muBkg/Run.TwoMuBkgSkim.C
@@ -10,10 +10,10 @@
 
   TDSet* dataset = new TDSet("TTree", "JPsi Bkg Tree", "rootuple");
   //
-  dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/2mu2k_BCDEF_2017.root");
+  dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/2muBkg_miniaod_17Nov2017_BCDEF_2017.root");
 
   // dataset->Add("/Users/adrianodiflorio/Documents/Git/X4140/ProofLite/Y4140_testrootuple.root");
-  TString selector = "TwoMuBkgSkim";
+  TString selector = "/lustre/home/adrianodif/jpsiphi/analysis/utilities/skimmers/2muBkg/TwoMuBkgSkim";
   TProof *p = TProof::Open("workers=40"); // 12 workers for qsub
 
   // Processing
