af543a6baf76998364f5a726fc8d832ef22927f7
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-03-22 17:26:21 +0100

    DiMuonDiTrakML

--- jpsiphi/jpsiphi/src/DiMuonDiTrakML2.cc
+++ jpsiphi/jpsiphi/src/DiMuonDiTrakML2.cc
@@ -1,190 +1,190 @@
-// -*- C++ -*-
+// // -*- C++ -*-
+// //
+// // Package:    Demo/DemoAnalyzer
+// // Class:      DemoAnalyzer
+// //
+// /**\class DemoAnalyzer DemoAnalyzer.cc Demo/DemoAnalyzer/plugins/DemoAnalyzer.cc
 //
-// Package:    Demo/DemoAnalyzer
-// Class:      DemoAnalyzer
+//  Description: [one line class summary]
 //
-/**\class DemoAnalyzer DemoAnalyzer.cc Demo/DemoAnalyzer/plugins/DemoAnalyzer.cc
-
- Description: [one line class summary]
-
- Implementation:
-     [Notes on implementation]
-*/
-//
-// Original Author:  Jhovanny Andres Mejia Guisao
-//         Created:  Tue, 05 Sep 2017 22:37:14 GMT
+//  Implementation:
+//      [Notes on implementation]
+// */
+// //
+// // Original Author:  Jhovanny Andres Mejia Guisao
+// //         Created:  Tue, 05 Sep 2017 22:37:14 GMT
+// //
+// //
 //
 //
-
-
-// system include files
-#include <memory>
-
-// user include files
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-
-#include "DataFormats/PatCandidates/interface/PackedCandidate.h" // muy importante para MiniAOD
-
-#include "FWCore/ServiceRegistry/interface/Service.h"
-#include "CommonTools/UtilAlgos/interface/TFileService.h"
-
-
+// // system include files
+// #include <memory>
 //
-// class declaration
+// // user include files
+// #include "FWCore/Framework/interface/Frameworkfwd.h"
+// #include "FWCore/Framework/interface/EDAnalyzer.h"
+// #include "FWCore/Framework/interface/Event.h"
+// #include "FWCore/Framework/interface/MakerMacros.h"
+// #include "FWCore/ParameterSet/interface/ParameterSet.h"
 //
-class DemoAnalyzer : public edm::EDAnalyzer {
-   public:
-      explicit DemoAnalyzer(const edm::ParameterSet&);
-      ~DemoAnalyzer();
-
-      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
-
-
-   private:
-      virtual void beginJob() override;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
-      virtual void endJob() override;
-
-      //virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
-      //virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
-      //virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
-      //virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
-
-
-      // ----------member data ---------------------------
-      edm::EDGetTokenT<edm::View<pat::PackedCandidate>> trackTags_;
-      TH1D *histo;
-
-};
-
+// #include "DataFormats/PatCandidates/interface/PackedCandidate.h" // muy importante para MiniAOD
 //
-// constants, enums and typedefs
+// #include "FWCore/ServiceRegistry/interface/Service.h"
+// #include "CommonTools/UtilAlgos/interface/TFileService.h"
 //
-
 //
-// static data member definitions
+// //
+// // class declaration
+// //
+// class DemoAnalyzer : public edm::EDAnalyzer {
+//    public:
+//       explicit DemoAnalyzer(const edm::ParameterSet&);
+//       ~DemoAnalyzer();
 //
-
+//       static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 //
-// constructors and destructor
 //
-DemoAnalyzer::DemoAnalyzer(const edm::ParameterSet& iConfig) :
-trackTags_(consumes<edm::View<pat::PackedCandidate>>(iConfig.getParameter<edm::InputTag>("tracks")))
-{
-   //now do what ever initialization is needed
-  edm::Service<TFileService> fs;
-  histo = fs->make<TH1D>("pt" , "Pt" , 50 , 0 , 50 );
-}
-
-
-DemoAnalyzer::~DemoAnalyzer()
-{
-
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
-
-}
-
-
+//    private:
+//       virtual void beginJob() override;
+//       virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+//       virtual void endJob() override;
 //
-// member functions
+//       //virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
+//       //virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
+//       //virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+//       //virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
 //
-
-// ------------ method called for each event  ------------
-void
-DemoAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
-{
-
-   using namespace edm;
-   using namespace std;
-
-   //std::cout<< " number of tracks:   " <<endl;
-
-
-   //Handle<TrackCollection> tracks;
-   //iEvent.getByLabel(trackTags_,tracks);
-   edm::Handle< View<pat::PackedCandidate> > tracks;
-   iEvent.getByToken(trackTags_,tracks);
-
-   //std::cout<< " number of tracks:   " << tracks->size() << endl;
-   LogInfo("Demo") << "number of tracks "<<tracks->size();
-
-   for(View<pat::PackedCandidate>::const_iterator itTrack = tracks->begin();
-       itTrack != tracks->end(); ++itTrack )
-     {
-       histo->Fill(itTrack->pt());
-     }
-
-#ifdef THIS_IS_AN_EVENT_EXAMPLE
-   Handle<ExampleData> pIn;
-   iEvent.getByLabel("example",pIn);
-#endif
-
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-#endif
-
-}
-
-
-// ------------ method called once each job just before starting event loop  ------------
-void
-DemoAnalyzer::beginJob()
-{
-}
-
-// ------------ method called once each job just after ending the event loop  ------------
-void
-DemoAnalyzer::endJob()
-{
-}
-
-// ------------ method called when starting to processes a run  ------------
-/*
-void
-DemoAnalyzer::beginRun(edm::Run const&, edm::EventSetup const&)
-{
-}
-*/
-
-// ------------ method called when ending the processing of a run  ------------
-/*
-void
-DemoAnalyzer::endRun(edm::Run const&, edm::EventSetup const&)
-{
-}
-*/
-
-// ------------ method called when starting to processes a luminosity block  ------------
-/*
-void
-DemoAnalyzer::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
-{
-}
-*/
-
-// ------------ method called when ending the processing of a luminosity block  ------------
-/*
-void
-DemoAnalyzer::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
-{
-}
-*/
-
-// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
-void
-DemoAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-  //The following says we do not know what parameters are allowed so do no validation
-  // Please change this to state exactly what you do use, even if it is no parameters
-  edm::ParameterSetDescription desc;
-  desc.setUnknown();
-  descriptions.addDefault(desc);
-}
-
-//define this as a plug-in
-DEFINE_FWK_MODULE(DemoAnalyzer);
+//
+//       // ----------member data ---------------------------
+//       edm::EDGetTokenT<edm::View<pat::PackedCandidate>> trackTags_;
+//       TH1D *histo;
+//
+// };
+//
+// //
+// // constants, enums and typedefs
+// //
+//
+// //
+// // static data member definitions
+// //
+//
+// //
+// // constructors and destructor
+// //
+// DemoAnalyzer::DemoAnalyzer(const edm::ParameterSet& iConfig) :
+// trackTags_(consumes<edm::View<pat::PackedCandidate>>(iConfig.getParameter<edm::InputTag>("tracks")))
+// {
+//    //now do what ever initialization is needed
+//   edm::Service<TFileService> fs;
+//   histo = fs->make<TH1D>("pt" , "Pt" , 50 , 0 , 50 );
+// }
+//
+//
+// DemoAnalyzer::~DemoAnalyzer()
+// {
+//
+//    // do anything here that needs to be done at desctruction time
+//    // (e.g. close files, deallocate resources etc.)
+//
+// }
+//
+//
+// //
+// // member functions
+// //
+//
+// // ------------ method called for each event  ------------
+// void
+// DemoAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+// {
+//
+//    using namespace edm;
+//    using namespace std;
+//
+//    //std::cout<< " number of tracks:   " <<endl;
+//
+//
+//    //Handle<TrackCollection> tracks;
+//    //iEvent.getByLabel(trackTags_,tracks);
+//    edm::Handle< View<pat::PackedCandidate> > tracks;
+//    iEvent.getByToken(trackTags_,tracks);
+//
+//    //std::cout<< " number of tracks:   " << tracks->size() << endl;
+//    LogInfo("Demo") << "number of tracks "<<tracks->size();
+//
+//    for(View<pat::PackedCandidate>::const_iterator itTrack = tracks->begin();
+//        itTrack != tracks->end(); ++itTrack )
+//      {
+//        histo->Fill(itTrack->pt());
+//      }
+//
+// #ifdef THIS_IS_AN_EVENT_EXAMPLE
+//    Handle<ExampleData> pIn;
+//    iEvent.getByLabel("example",pIn);
+// #endif
+//
+// #ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
+//    ESHandle<SetupData> pSetup;
+//    iSetup.get<SetupRecord>().get(pSetup);
+// #endif
+//
+// }
+//
+//
+// // ------------ method called once each job just before starting event loop  ------------
+// void
+// DemoAnalyzer::beginJob()
+// {
+// }
+//
+// // ------------ method called once each job just after ending the event loop  ------------
+// void
+// DemoAnalyzer::endJob()
+// {
+// }
+//
+// // ------------ method called when starting to processes a run  ------------
+// /*
+// void
+// DemoAnalyzer::beginRun(edm::Run const&, edm::EventSetup const&)
+// {
+// }
+// */
+//
+// // ------------ method called when ending the processing of a run  ------------
+// /*
+// void
+// DemoAnalyzer::endRun(edm::Run const&, edm::EventSetup const&)
+// {
+// }
+// */
+//
+// // ------------ method called when starting to processes a luminosity block  ------------
+// /*
+// void
+// DemoAnalyzer::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+// {
+// }
+// */
+//
+// // ------------ method called when ending the processing of a luminosity block  ------------
+// /*
+// void
+// DemoAnalyzer::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+// {
+// }
+// */
+//
+// // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+// void
+// DemoAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+//   //The following says we do not know what parameters are allowed so do no validation
+//   // Please change this to state exactly what you do use, even if it is no parameters
+//   edm::ParameterSetDescription desc;
+//   desc.setUnknown();
+//   descriptions.addDefault(desc);
+// }
+//
+// //define this as a plug-in
+// DEFINE_FWK_MODULE(DemoAnalyzer);
