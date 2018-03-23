d93584e722a43dd3535baac7bd6f0898cc372161
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-03-23 17:56:14 +0100

    /lustre/cms/store/user/adiflori/Charmonium/crab_miniaod_2k2Trig_Charmonium_Run2017F-17Nov2017-v1_MINIAOD_305388-309000__20180322_150955/merge.root

--- utilities/skimmers/w_hlts/Run.TwoMuTwoTrakTrigSkimVertex.C
+++ utilities/skimmers/w_hlts/Run.TwoMuTwoTrakTrigSkimVertex.C
@@ -10,7 +10,7 @@
 
   TDSet* dataset = new TDSet("TTree", "JPsi Phi Tree", "DiMuonDiTrakRootuplerHLT");
   //
-  dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/crab_miniaod_2k2Trig_Charmonium_Run2017F-17Nov2017-v1_MINIAOD_305388-309000__20180301_001621/merge.root");
+  dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/crab_miniaod_2k2Trig_Charmonium_Run2017F-17Nov2017-v1_MINIAOD_305388-309000__20180322_150955/merge.root");
 
   // dataset->Add("/Users/adrianodiflorio/Documents/Git/X4140/ProofLite/Y4140_testrootuple.root");
   TString selector = "DiMuonDiTrigVertex";
