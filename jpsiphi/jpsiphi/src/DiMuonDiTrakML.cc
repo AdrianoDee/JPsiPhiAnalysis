6e6795bf48f4b1680680f8867527652d0aeffaf9
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-03-23 19:03:52 +0100

    Testing DiMuonFit

--- jpsiphi/jpsiphi/src/DiMuonDiTrakML.cc
+++ jpsiphi/jpsiphi/src/DiMuonDiTrakML.cc
@@ -3,7 +3,11 @@
 
 DiMuonDiTrakML::DiMuonDiTrakML(const edm::ParameterSet& iConfig):
 muons_(consumes<reco::MuonCollection>(iConfig.getParameter<edm::InputTag>("Muons"))),
-traks_(consumes<reco::TrackCollection>(iConfig.getParameter<edm::InputTag>("Tracks")))
+traks_(consumes<reco::TrackCollection>(iConfig.getParameter<edm::InputTag>("Tracks"))),
+thebeamspot_(consumes<reco::BeamSpot>(iConfig.getParameter<edm::InputTag>("BeamSpot"))),
+thePVs_(consumes<reco::VertexCollection>(iConfig.getParameter<edm::InputTag>("PrimaryVertex"))),
+DiMuonMassCuts_(iConfig.getParameter<std::vector<double>>("DiMuonCuts")),
+DiTrakMassCuts_(iConfig.getParameter<std::vector<double>>("DiTrakCuts")),
 // thebeamspot_(consumes<reco::BeamSpot>(iConfig.getParameter<edm::InputTag>("BeamSpot"))),
 // thePVs_(consumes<reco::VertexCollection>(iConfig.getParameter<edm::InputTag>("PrimaryVertex"))),
 // DiMuonMassCuts_(iConfig.getParameter<std::vector<double>>("DiMuonCuts")),
@@ -33,8 +37,107 @@ DiMuonDiTrakML::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   edm::Handle<reco::MuonCollection> muons;
   iEvent.getByToken(muons_,muons);
 
+  Vertex thePrimaryV;
+
+  ESHandle<MagneticField> magneticField;
+  iSetup.get<IdealMagneticFieldRecord>().get(magneticField);
+  const MagneticField* field = magneticField.product();
+
+  edm::Handle<BeamSpot> theBeamSpot;
+  iEvent.getByToken(thebeamspot_,theBeamSpot);
+  BeamSpot bs = *theBeamSpot;
+
+  edm::Handle<VertexCollection> priVtxs;
+  iEvent.getByToken(thePVs_, priVtxs);
+  if ( priVtxs->begin() != priVtxs->end() ) {
+    thePrimaryV = Vertex(*(priVtxs->begin()));
+  }
+  else {
+    thePrimaryV = Vertex(bs.position(), bs.covariance3D());
+  }
+
+
+  edm::ESHandle<TransientTrackBuilder> theTTBuilder;
+  iSetup.get<TransientTrackRecord>().get("TransientTrackBuilder",theTTBuilder);
+  KalmanVertexFitter vtxFitter(true);
+
   int max = 0;
-  for(reco::TrackCollection::const_iterator itTrack = tracks->begin();itTrack != tracks->end(); ++itTrack )
+
+  float vProb, vNDF, vChi2, minDz = 999999.;
+  float cosAlpha, ctauPV, ctauErrPV, dca;
+  float l_xy, lErr_xy;
+
+  for(reco::MuonCollection::const_iterator mPos = muons->begin();mPos != muons->end(); ++mPos )
+  {
+    if(mPos->charge()<=0.0) continue;
+    if (!(mPos->bestTrack().isNonnull())) continue;
+    if (!(mPos->innerTrack().isNonnull())) continue;
+
+    for(reco::MuonCollection::const_iterator mNeg = muons->begin();mNeg != muons->end(); ++mNeg )
+    {
+      if(mNeg->charge()>=0.0) continue;
+      if (!(mNeg->bestTrack().isNonnull())) continue;
+      if (!(mNeg->innerTrack().isNonnull())) continue;
+
+      vector<TransientVertex> vDiMuon;
+
+      LorentzVector mumu = mNeg->p4() + mPos->p4();
+
+      TLorentzVector mu1, mu2,mumuP4;
+
+      mu1.SetXYZM(mNeg->track()->px(),mNeg->track()->py(),mNeg->track()->pz(),muon_mass);
+      mu2.SetXYZM(mPos->track()->px(),mPos->track()->py(),mPos->track()->pz(),muon_mass);
+
+      mumuP4=mu1+mu2;
+      mumucand.setP4(mumu);
+      mumucand.setCharge(mNeg->charge()+mPos->charge());
+
+      if(mumu.M() < DiMuonMassCuts_[1]) continue;
+      if(mumu.M() > DiMuonMassCuts_[0]) continue;
+
+      std::vector<TransientTrack> mm_ttks;
+
+      mm_ttks.push_back(theTTBuilder->build(mNeg->track()));  // pass the reco::Track, not  the reco::TrackRef (which can be transient)
+      mm_ttks.push_back(theTTBuilder->build(mPos->track()));
+
+      TransientVertex mumuVertex = vtxFitter.vertex(mm_ttks);
+      CachingVertex<5> VtxForInvMass = vtxFitter.vertex( mm_ttks );
+
+      Measurement1D MassWErr(mPos->mass(),-9999.);
+      if ( field->nominalValue() > 0 )
+          MassWErr = massCalculator.invariantMass( VtxForInvMass, mmMasses );
+      else
+          mumuVertex = TransientVertex();                      // with no arguments it is invalid
+
+      if (!(mumuVertex.isValid()))
+          continue;
+
+      vChi2 = mumuVertex.totalChiSquared();
+      vNDF  = mumuVertex.degreesOfFreedom();
+      vProb = TMath::Prob(vChi2,(int)vNDF);
+      for(reco::TrackCollection::const_iterator posTrack = tracks->begin();posTrack != tracks->end(); ++posTrack )
+      {
+        if(posTrack->charge()<=0.0) continue;
+        if(!(posTrack.isNonnull())) continue;
+
+        for(reco::TrackCollection::const_iterator negTrack = tracks->begin();negTrack != tracks->end(); ++negTrack )
+        {
+          if(negTrack->charge()>=0.0) continue;
+          if(!(negTrack.isNonnull())) continue;
+
+
+        }
+
+
+      }
+
+
+    }
+
+  }
+    for(reco::TrackCollection::const_iterator itTrack = tracks->begin();itTrack != tracks->end(); ++itTrack )
+      for(reco::TrackCollection::const_iterator itTrack = tracks->begin();itTrack != tracks->end(); ++itTrack )
+        for(reco::TrackCollection::const_iterator itTrack = tracks->begin();itTrack != tracks->end(); ++itTrack )
     max = std::max(max,itTrack->numberOfValidHits());
 
   std::cout<<max<<std::endl;
