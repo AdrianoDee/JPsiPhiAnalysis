8815e10d01ff99ebf28686e32d2495e4c23ed22b
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-06-21 00:22:02 +0200

    Send Y

---
+++ jpsiphi/jpsiphi/test/sendYMC.sh
@@ -0,0 +1,17 @@
+
+cmsRun run-4mu-Y2017-miniaod.py yMass 4100 --numThreads 4 &
+
+cmsRun run-4mu-Y2017-miniaod.py yMass 4300 --numThreads 4 &
+
+cmsRun run-4mu-Y2017-miniaod.py yMass 4500 --numThreads 4 &
+
+cmsRun run-4mu-Y2017-miniaod.py yMass 4700 --numThreads 4 &
+
+cmsRun run-2mu2k-Y2017-miniaod.py yMass 4100 --numThreads 4 &
+
+cmsRun run-2mu2k-Y2017-miniaod.py yMass 4300 --numThreads 4 &
+
+cmsRun run-2mu2k-Y2017-miniaod.py yMass 4500 --numThreads 4 &
+
+cmsRun run-2mu2k-Y2017-miniaod.py yMass 4700 --numThreads 4 &
+
