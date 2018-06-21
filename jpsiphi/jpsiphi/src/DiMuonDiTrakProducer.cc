fd1bf65bca92823fffbb721eb1c2289c4d9bf6be
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-06-21 18:28:38 +0200

    x_en_fit

--- jpsiphi/jpsiphi/src/DiMuonDiTrakProducer.cc
+++ jpsiphi/jpsiphi/src/DiMuonDiTrakProducer.cc
@@ -295,11 +295,11 @@ void DiMuonDiTrakProducer::produce(edm::Event& iEvent, const edm::EventSetup& iS
            double x_px_fit = fitX->currentState().kinematicParameters().momentum().x();
            double x_py_fit = fitX->currentState().kinematicParameters().momentum().y();
            double x_pz_fit = fitX->currentState().kinematicParameters().momentum().z();
-           double x_en_fit = sqrt(x_ma_fit*x_ma_fit+x_px_fit*x_px_fit+
+           // double x_en_fit = sqrt(x_ma_fit*x_ma_fit+x_px_fit*x_px_fit+
                                      x_py_fit*x_py_fit+x_pz_fit*x_pz_fit);
            double x_vx_fit = fitXVertex->position().x();
 	         double x_vy_fit = fitXVertex->position().y();
-           double x_vz_fit = fitXVertex->position().z();
+           // double x_vz_fit = fitXVertex->position().z();
 
            vtx.SetXYZ(x_vx_fit,x_vy_fit,0);
            TVector3 pperp(x_px_fit, x_py_fit, 0);
