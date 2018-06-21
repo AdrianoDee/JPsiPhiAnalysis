ec863d68aeeff01dbde23465302d8d9f5afa34d2
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-06-21 08:59:37 +0200

    New send

--- jpsiphi/jpsiphi/test/sendYMC.sh
+++ jpsiphi/jpsiphi/test/sendYMC.sh
@@ -1,16 +1,16 @@
 
-cmsRun run-4mu-Y2017-miniaod.py --numThreads 4 yMass=4100 &
+cmsRun --numThreads 4 run-4mu-Y2017-miniaod.py yMass=4100 &
 
-cmsRun run-4mu-Y2017-miniaod.py --numThreads 4 yMass=4300 &
+cmsRun --numThreads 4 run-4mu-Y2017-miniaod.py yMass=4300 &
 
-cmsRun run-4mu-Y2017-miniaod.py --numThreads 4 yMass=4500 &
+cmsRun --numThreads 4 run-4mu-Y2017-miniaod.py yMass=4500 &
 
-cmsRun run-4mu-Y2017-miniaod.py --numThreads 4 yMass=4700 &
+cmsRun --numThreads 4 run-4mu-Y2017-miniaod.py yMass=4700 &
 
-cmsRun run-2mu2k-Y2017-miniaod.py --numThreads 4 yMass=4100 &
+cmsRun --numThreads 4 run-2mu2k-Y2017-miniaod.py yMass=4100 &
 
-cmsRun run-2mu2k-Y2017-miniaod.py --numThreads 4 yMass=4300 &
+cmsRun --numThreads 4 run-2mu2k-Y2017-miniaod.py yMass=4300 &
 
-cmsRun run-2mu2k-Y2017-miniaod.py --numThreads 4 yMass=4500 &
+cmsRun --numThreads 4 run-2mu2k-Y2017-miniaod.py yMass=4500 &
 
-cmsRun run-2mu2k-Y2017-miniaod.py --numThreads 4 yMass=4700 &
+cmsRun --numThreads 4 run-2mu2k-Y2017-miniaod.py yMass=4700 &
