6ef422430715d9bfd49a2e6eb984d0f8b668281a
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-07-12 01:51:24 +0300

    std::cout << status << std::endl;

--- jpsiphi/jpsiphi/src/DiMuonDiTrakProducerFit.cc
+++ jpsiphi/jpsiphi/src/DiMuonDiTrakProducerFit.cc
@@ -451,7 +451,7 @@ void DiMuonDiTrakProducerFit::produce(edm::Event& iEvent, const edm::EventSetup&
 	         double x_vy_fit = fitXVertex->position().y();
            double x_vz_fit = fitXVertex->position().z();
            vtx.SetXYZ(x_vx_fit,x_vy_fit,0);
-           std::cout << status << std::endl;
+
            bool status = ttmd.calculate( GlobalTrajectoryParameters(
              GlobalPoint(x_vx_fit,x_vy_fit,x_vz_fit),
              GlobalVector(x_px_fit,x_py_fit,x_pz_fit),TrackCharge(0),&(*magneticField)),
@@ -459,6 +459,7 @@ void DiMuonDiTrakProducerFit::produce(edm::Event& iEvent, const edm::EventSetup&
                GlobalPoint(bs.position().x(), bs.position().y(), bs.position().z()),
                GlobalVector(bs.dxdz(), bs.dydz(), 1.),TrackCharge(0),&(*magneticField)));
            float extrapZ=-9E20;
+           std::cout << status << std::endl;
            if (status) extrapZ=ttmd.points().first.z();
            std::cout << status << std::endl;
            TVector3 pperp(x_px_fit, x_py_fit, 0);
