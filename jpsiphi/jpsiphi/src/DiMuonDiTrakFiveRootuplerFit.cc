2f3fb7fdb0727f773f540ad10059986a9f1f29fb
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-07-15 04:41:29 +0200

    reco::BeamSpot bs = *theBeamSpot;

--- jpsiphi/jpsiphi/src/DiMuonDiTrakFiveRootuplerFit.cc
+++ jpsiphi/jpsiphi/src/DiMuonDiTrakFiveRootuplerFit.cc
@@ -819,7 +819,7 @@ if ( (isMC_ || OnlyGen_) && packed.isValid() && pruned.isValid() ) {
 
 if(OnlyGen_) dimuonditrk_tree->Fill();
 
-std::map <unsigned int,const pat::CompositeCandidate*> fourToFiveMap;
+std::map <int,const pat::CompositeCandidate*> fourToFiveMap;
 
 if(!OnlyGen_)
 {
@@ -828,7 +828,7 @@ if(!OnlyGen_)
   if (dimuonditrk_cand_handle.isValid() && fivetracks_cand_hanlde.isValid())
   {
     for (unsigned int i=0; i< fivetracks_cand_hanlde->size(); i++)
-      fourToFiveMap[fivetracks_cand_hanlde->at(i).userInt("index")] = &(fivetracks_cand_hanlde->at(i));
+      fourToFiveMap[int(fivetracks_cand_hanlde->at(i).userInt("index"))] = &(fivetracks_cand_hanlde->at(i));
   }
 }
 
@@ -874,7 +874,7 @@ if(!OnlyGen_)
       fifthtrak_phi = -1.0;
       fifthtrak_y = -1.0;
 
-      if(std::find(fourToFiveMap.begin(),fourToFiveMap.end(),i)!=fourToFiveMap.end())
+      if(std::find(fourToFiveMap.begin(),fourToFiveMap.end(),int(i))!=fourToFiveMap.end())
       {
         fivetrak_cand = fourToFiveMap[i];
         dimuontrak_cand = dynamic_cast <pat::CompositeCandidate *>(dimuonditrk_cand.daughter("dimuontrak"));
