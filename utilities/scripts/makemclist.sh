cc83dbede99241c980cec20730646d8d41d9c35f
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-10-28 14:50:11 +0100

    cding

--- utilities/scripts/makemclist.sh
+++ utilities/scripts/makemclist.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-if [[ $# -eq 0 ]] ; then
-    echo 'No filename given!'
+if [[ $# -leq 1 ]] ; then
+    echo 'No filename (1) and/or dir(2) given!'
     exit 0
 fi
 
+
 f=$1
+d=$2
 
-echo "Building python library with MC file list for ${f}"
+echo "Building python library with MC file list for ${f} in ${d}"
 
+cd $d
 
 for ff in crab*;
 do
@@ -28,3 +31,5 @@ git add /lustre/home/adrianodif/jpsiphi/2018/CMSSW_10_2_1/src/jpsiphi/jpsiphi/te
 
 git commit -m "Adding ${f} MC list"
 git push
+
+cd -
