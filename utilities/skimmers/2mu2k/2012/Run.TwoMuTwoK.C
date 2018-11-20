dee979fbf9dbfd4125d29ea1b0200cf0136a37c8
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-11-20 18:35:13 +0100

    Up

--- utilities/skimmers/2mu2k/2012/Run.TwoMuTwoK.C
+++ utilities/skimmers/2mu2k/2012/Run.TwoMuTwoK.C
@@ -8,12 +8,14 @@
 
   // INPUT DATA SAMPLE ON LOCAL DISK
 
+  TDSet* dataset = new TDSet("TTree", "JPsiPhiTree", "rootuple");
+
+  /// 2018
+
   dataset->Add("/lustre/home/adrianodif/MuOniaParked/2012_data/runB_resplit_Oct17/runB_split_14.root");
 
-   TString selector = "/lustre/home/adrianodif/jpsiphi/2018/data_2018/analysis/utilities/skimmers/2mu2k/2012/TwoMuTwoK_2012";
+     TString selector = "/lustre/home/adrianodif/jpsiphi/2018/data_2018/analysis/utilities/skimmers/2mu2k/2012/TwoMuTwoK_2012";
 
-
-  TString selector = "/lustre/home/adrianodif/jpsiphi/2018/data_2018/analysis/utilities/skimmers/2mu2k/TwoMuTwoK";
   TProof *p = TProof::Open("workers=5"); // 12 workers for qsub
   //gProofDebugMask = TProofDebug::kAll;
   //gProofDebugLevel = 5;
