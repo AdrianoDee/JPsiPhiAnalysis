23fd769012637bc7e08936af855b6e3699061cb6
AdrianoDee <adriano.diflorio@ba.infn.it>
2018-11-22 02:50:36 +0100

    NCpu

--- utilities/splots/xsPlot.py
+++ utilities/splots/xsPlot.py
@@ -16,7 +16,7 @@ from string import split
 from ROOT import RooRealVar,RooAbsPdf,RooChebychev,RooExponential,RooGaussian,TLine
 from ROOT import RooBernstein,RooAbsPdf,RooPlot,RooAddPdf,RooDataHist,RooArgSet,RooArgList
 from ROOT import kGreen,kRed,kBlack,kBlue,kDashed,kDotted,kMagenta,RooVoigtian
-from ROOT.RooFit import Components,LineColor,LineStyle,Name,Normalization,Range,SelectVars
+from ROOT.RooFit import Components,LineColor,LineStyle,Name,Normalization,Range,SelectVars, Title
 from ROOT import RooDataSet,RooFormulaVar,RooLinkedList
 
 parser = argparse.ArgumentParser()
@@ -46,7 +46,7 @@ binning = 400
 massbins = (phimax - phimean)/0.005
 dimuonditrk_m_rf_c = RooRealVar("dimuonditrk_m_rf_c","M(#mu#muKK)[GeV]",4.0,6.0)
 dimuonditrk_m_rf_c.setBins(binning)
-masskk = RooRealVar("ditrak_m","M(KK)",0.99,1.05);
+masskk = RooRealVar("ditrak_m","M(KK) [GeV]",1.00,1.045);
 masskk.setBins(int(200))
 
 dimuonditrk_ctauPV    = RooRealVar("dimuonditrk_ctauPV","dimuonditrk_ctauPV",-1000.0,1000.0)
@@ -61,22 +61,22 @@ print "Tree entries %d"%(splotData.numEntries())
 
 
 
-a0 = RooRealVar("a0","a0",0.001,-10.,10.)
-a1 = RooRealVar("a1","a1",0.001,-5.0,5.0)
-a2 = RooRealVar("a2","a2",-0.00001,-2.,2.)
-a3 = RooRealVar("a3","a3",0.0,-0.5,0.5)
-a4 = RooRealVar("a4","a4",0.0,-0.2,0.2)
+a0 = RooRealVar("a_{0}","a0",0.001,-10.,10.)
+a1 = RooRealVar("a_{1}","a1",0.001,-5.0,5.0)
+a2 = RooRealVar("a_{2}","a2",-0.00001,-2.,2.)
+a3 = RooRealVar("a_{3}","a3",0.0,-0.5,0.5)
+a4 = RooRealVar("a_{4}","a4",0.0,-0.2,0.2)
 a5 = RooRealVar("a5","a5",0.0,-0.025,0.05)
 a6 = RooRealVar("a6","a6",0.0,-0.001,0.001)
 
 aset = RooArgList(a0,a1,a2,a3,a4,a5)
 
-sigma = RooRealVar("sigma","width of gaussian",0.01,0.005,0.05)
+sigma = RooRealVar("#sigma","width of gaussian",0.01,0.001,0.05)
 gamma = RooRealVar("#Gamma","gamma of bw",0.0042,0.001,0.01)
-mean = RooRealVar("mean","mean of gaussian",phimean,phimean-0.1,phimean+0.1);
+mean = RooRealVar("#mu","mean of gaussian",phimean,phimean-0.1,phimean+0.1);
 
-nSig = RooRealVar("nSig","nSig",float(nentries)*0.1,0.,float(nentries))
-nBkg = RooRealVar("nBkg","nBkg",float(nentries)*0.9,0.,float(nentries))
+nSig = RooRealVar("n_{Sig}","nSig",float(nentries)*0.1,0.,float(nentries))
+nBkg = RooRealVar("n_{Bkg}","nBkg",float(nentries)*0.9,0.,float(nentries))
 cheb = RooChebychev("cheb","Background",masskk,aset)
 gauss = RooGaussian("gauss","gaussian PDF ",masskk,mean,sigma)
 #signal = RooVoigtian("signal","signal",psiPrimeMass,mean,gamma,sigma)
@@ -109,7 +109,7 @@ rPhifit = tot.fitTo(splotData,Range(phimin,phimax),RooFit.NumCPU(args.ncpu),RooF
 nfits = nfits + 1
 
 c = TCanvas("canvas","canvas",1200,800)
-phiFrame = masskk.frame(Range(phimin,phimax),Normalization((nSig.getValV() + nBkg.getValV())))
+phiFrame = masskk.frame(Range(phimin,phimax),Normalization((nSig.getValV() + nBkg.getValV())), Title("#Phi Mass"))
 splotData.plotOn(phiFrame)
 ratio = 1.0/float(nfits)
 
@@ -134,8 +134,8 @@ lineup.SetLineColor(kGreen)
 linelow.SetLineWidth(2)
 lineup.SetLineWidth(2)
 
-linelow.Draw()
-lineup.Draw()
+#linelow.Draw()
+#lineup.Draw()
 
 #tot.paramOn(phiFrame,RooFit.Layout(0.57,0.99,0.65))
 
