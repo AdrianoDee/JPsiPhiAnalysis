afbc8e30be430bff37078aac49302bde820fb12e
AdrianoDee <adriano.di.florio@cern.ch>
2019-03-24 00:29:50 +0100

    D_six

---
+++ utilities/skimmers/sixtracks/Run.SixTracks.C
@@ -0,0 +1,39 @@
+{
+
+#include "TTree.h"
+#include "TDSet.h"
+#include "TProof.h"
+#include "TString.h"
+
+
+  // INPUT DATA SAMPLE ON LOCAL DISK
+
+  TDSet* dataset = new TDSet("TTree", "SixTracksTree", "rootupleSix");
+
+  /// 2018
+  //D
+  /*
+  dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/crab_miniaod_2mu2k_five_Charmonium_Run2018D-PromptReco-v2_MINIAOD___20190323_105234/190323_095241/0000/sum.root");
+  dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/crab_miniaod_2mu2k_five_Charmonium_Run2018D-PromptReco-v2_MINIAOD___20190323_105234/190323_095241/0001/sum.root");
+  dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/crab_miniaod_2mu2k_five_Charmonium_Run2018D-PromptReco-v2_MINIAOD___20190323_105234/190323_095241/0002/sum.root");
+  dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/crab_miniaod_2mu2k_five_Charmonium_Run2018D-PromptReco-v2_MINIAOD___20190323_105234/190323_095241/0003/sum.root");
+  dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/crab_miniaod_2mu2k_five_Charmonium_Run2018D-PromptReco-v2_MINIAOD___20190323_105234/190323_095241/0004/sum.root");
+  dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/crab_miniaod_2mu2k_five_Charmonium_Run2018D-PromptReco-v2_MINIAOD___20190323_105234/190323_095241/0005/sum.root");
+  dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/crab_miniaod_2mu2k_five_Charmonium_Run2018D-PromptReco-v2_MINIAOD___20190323_105234/190323_095241/0006/sum.root");
+  dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/crab_miniaod_2mu2k_five_Charmonium_Run2018D-PromptReco-v2_MINIAOD___20190323_105234/190323_095241/0007/sum.root");
+  dataset->Add("/lustre/cms/store/user/adiflori/Charmonium/crab_miniaod_2mu2k_five_Charmonium_Run2018D-PromptReco-v2_MINIAOD___20190323_105234/190323_095241/0008/sum.root");
+*/
+
+  TString selector = "/lustre/home/adrianodif/jpsiphi/2018/data_2018/analysis/utilities/skimmers/sixtracks/SixTracks";
+  TProof *p = TProof::Open("workers=5"); // 12 workers for qsub
+  //gProofDebugMask = TProofDebug::kAll;
+  //gProofDebugLevel = 5;
+
+  // Processing
+  cout << ">> Processing " << selector << " ... " << endl;
+
+  TString selectorplus = selector;
+  selectorplus += ".C+";
+  p->Process(dataset, selectorplus);
+
+}
