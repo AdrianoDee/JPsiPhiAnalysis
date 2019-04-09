68e6dd7519adc68d7a5b44a2cc7d85331448abc2
AdrianoDee <adriano.di.florio@cern.ch>
2019-04-09 18:04:07 +0200

    MMKK

--- utilities/skimmers/sixtracks/SixTracks.C
+++ utilities/skimmers/sixtracks/SixTracks.C
@@ -537,8 +537,9 @@ Bool_t SixTracks::Process(Long64_t entry)
     out_dimuonDiTrkTwo_mmpp = 	(Float_t)(*dimuonDiTrkTwo_mmpp);
     out_dimuonDiTrkThree_mmpp = 	(Float_t)(*dimuonDiTrkThree_mmpp);
     out_dimuonDiTrkFour_mmpp = 	(Float_t)(*dimuonDiTrkFour_mmpp);
-    // out_dimuonDiTrkOne_mmkk = 	(Float_t)(*dimuonDiTrkOne_mmkk);
-    // out_dimuonDiTrkTwo_mmkk = 	(Float_t)(*dimuonDiTrkTwo_mmkk);
+
+    out_dimuonDiTrkOne_mmkk = 	(Float_t)(*dimuonDiTrkOne_mmkk);
+    out_dimuonDiTrkTwo_mmkk = 	(Float_t)(*dimuonDiTrkTwo_mmkk);
     // out_dimuonDiTrkThree_mmkk = 	(Float_t)(*dimuonDiTrkThree_mmkk);
     // out_dimuonDiTrkFour_mmkk = 	(Float_t)(*dimuonDiTrkFour_mmkk);
 
