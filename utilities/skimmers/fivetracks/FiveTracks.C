6a07a7c4495c72da972301b4fa21bee847d01199
AdrianoDee <adriano.di.florio@cern.ch>
2019-04-10 17:40:18 +0200

    Filtering

--- utilities/skimmers/fivetracks/FiveTracks.C
+++ utilities/skimmers/fivetracks/FiveTracks.C
@@ -362,7 +362,10 @@ Bool_t FiveTracks::Process(Long64_t entry)
    test = (*five_m_ref) > 4.5 && (*five_m_ref) < 6.0;
    test = (*lowMuonMatch) > 0.0 && (*lowTrack_pt) > 1.0 && (*thirdTrack_pt) >1.0;
 
-   if(true)
+   test = (*lowMuon_NPixelHits) > 0  && (*highMuon_NPixelHits§) > 0 ;
+   test = (*highTrack_NPixelHits§) > 0 && (*lowTrack_NPixelHits§) > 0 && (*thirdTrack_NPixelHits) > 0;
+
+   if(test)
    {
 
      out_run = 	(Float_t)(*run);
